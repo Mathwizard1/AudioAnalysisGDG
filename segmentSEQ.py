971e650603dcbd09df1bd2f57a1300e44e883a54
Mathwizard1 <anshurup.gupta@gmail.com>
2025-03-26 08:23:38 +0530

    .

--- segmentSEQ.py
+++ segmentSEQ.py
@@ -1,7 +1,6 @@
 import os
-import multiprocessing
-
 import numpy as np
+
 import csv
 
 import librosa
@@ -15,12 +14,6 @@ N_MFCC = 20
 
 OUTPUT_FOLDER = "processdata"
 INPUT_FOLDER = "data"
-
-FILE_PROCESS_LIMIT = 3
-PROCESS_LIMIT = 100 
-MEMORY_LIMIT = 150 * 1e+6   # In x MB
-
-MIN_AUDIO_LENGTH = 256  # If end parts are too small, skip them
 
 def feature_extract(y, sr, n_mfcc = 20):
     # Tempo information
@@ -67,36 +60,9 @@ def feature_extract(y, sr, n_mfcc = 20):
         flattened_list.extend(features_list[key])
     return flattened_list
 
-def writer_process(queue, file_path):
-    try:
-        with open(file_path, 'a', newline='\n') as csvfile:
-            writer = csv.writer(csvfile)
-            while True:
-                item = queue.get()
-                if item is None:  # Sentinel value to stop
-                    break
-                writer.writerow(item)
-    except Exception as e:
-        print(f"Writer process error: {e}")
-
-def data_processor(y, sr, genre, queue):
-    try:
-        X = feature_extract(y, sr, N_MFCC)
-        X.append(genre)
-        queue.put(X)
-
-        del X, y
-        gc.collect()
-    except Exception as e:
-        print(f"data Processor error: {e}")
-
 def segment_mp3(input_file, input_folder = INPUT_FOLDER, output_folder= OUTPUT_FOLDER):
     output_file = input_file.split('.')[0] + '.csv'
     genre = input_folder.split('\\')[-1]
-
-    queue = multiprocessing.Queue()
-    writer = multiprocessing.Process(target=writer_process, args=(queue, output_folder + "\\" + output_file))
-    writer.start()
 
     try:
         # print(input_folder + "\\" + input_file)
@@ -108,46 +74,29 @@ def segment_mp3(input_file, input_folder = INPUT_FOLDER, output_folder= OUTPUT_F
     interval_samples = sr * TIME_INTERVAL
     num_intervals = int(np.floor(len(y) / interval_samples))
 
-    processes = []
-    process_count = 0
-    for i in range(IGNORE_MIN, num_intervals + 1):
-        start_sample = i * interval_samples
-        end_sample = (i + 1) * interval_samples
-        interval_audio = None
+    with open(output_file, 'a', newline='\n') as csvfile:
+        writer = csv.writer(csvfile)
+        for i in range(IGNORE_MIN, num_intervals + 1):
+            start_sample = i * interval_samples
+            end_sample = (i + 1) * interval_samples
+            interval_audio = None
 
-        # Save the remaining part
-        if(i < num_intervals):
-            interval_audio = y[start_sample:end_sample]
-        else:
-            interval_audio = y[start_sample:]
+            # Save the remaining part
+            if(i < num_intervals):
+                interval_audio = y[start_sample:end_sample]
+            else:
+                interval_audio = y[start_sample:]
 
-        # Save the output
-        # output_file = output_folder + "\\" + input_file.split('.')[0] + f"_{i+1}.mp3"
-        # sf.write(output_file, interval_audio, sr)
+            # Save the output
+            # output_file = output_folder + "\\" + input_file.split('.')[0] + f"_{i+1}.mp3"
+            # sf.write(output_file, interval_audio, sr)
 
-        if(process_count == PROCESS_LIMIT):
-            for p in processes:
-                p.join()
-            processes.clear()
-            process_count = 0
+            X = feature_extract(y, sr, N_MFCC)
+            X.append(genre)
+            writer.writerow(X)
 
-        if(len(interval_audio) >= MIN_AUDIO_LENGTH):
-            p = multiprocessing.Process(target=data_processor, args=(interval_audio, sr, genre, queue), 
-                                        daemon= True)
-            process_count += 1
-
-            processes.append(p)        
-            p.start()
-
-        del interval_audio
-        gc.collect()
-
-    for p in processes:
-        p.join()
-
-    queue.put(None)  # Sentinel to signal writer to stop
-    writer.join()
-
+            del interval_audio, X
+            gc.collect()
     del y
     gc.collect()
     print(f"Audio file '{input_file}' split and saved to '{output_folder}' successfully.")
@@ -165,35 +114,9 @@ if __name__ == "__main__":
 
             for file in files:
                 # print(file)
-
-                # add the current memory Usage
-                file_size_limits += os.path.getsize(INPUT_FOLDER + "\\" + genre + "\\" + file)
-
-                # Limit memory
-                if(file_size_limits >= MEMORY_LIMIT or file_process_limits > FILE_PROCESS_LIMIT):
-                    print(file_size_limits / 1e+6)
-                    print(file_process_limits)
-
-                    for p in processes:
-                        p.join()
-                        gc.collect()
-
-                    processes.clear()
-                    file_process_limits = 0
-                    file_size_limits = os.path.getsize(INPUT_FOLDER + "\\" + genre + "\\" + file)
-
-                p = multiprocessing.Process(
-                    target= segment_mp3,
-                    args= (file, INPUT_FOLDER + "\\" + genre, OUTPUT_FOLDER + "\\" + genre)
-                )
-                file_process_limits += 1
-
-                processes.append(p)
-                p.start()
-
-            for p in processes:
-                p.join()
-                gc.collect()
+                segment_mp3(file, INPUT_FOLDER + "\\" + genre, OUTPUT_FOLDER + "\\" + genre)
+                
+            gc.collect()
 
         print("Genre:", genre)
     print("Segments done!")
